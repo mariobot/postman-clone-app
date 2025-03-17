190a94f1e01cf5bb080861a11167f9b92ca370af
Mario Andres Botero Florez <mariobot113@hotmail.com>
2025-03-17 11:04:43 -0500

    Add models project

---
+++ PostmanCloneModels/Request.cs
@@ -0,0 +1,26 @@
+﻿namespace PostmanCloneModels;
+
+public class Request
+{
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// The URL of the API to call
+    /// </summary>
+    public Uri? Url { get; set; }
+
+    /// <summary>
+    /// The body of the request
+    /// </summary>
+    public string? Body { get; set; }
+
+    /// <summary>
+    /// The HTTP method to use
+    /// </summary>
+    public required string Method { get; set; }
+
+    /// <summary>
+    /// The title of the request
+    /// </summary>
+    public string? Title { get; set; }
+}
