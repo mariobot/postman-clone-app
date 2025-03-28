1c66da4a73862a911364d79a8bb9da0bd2b4fcc0
Mario Andres Botero Florez <mariobot113@hotmail.com>
2025-03-28 18:45:42 -0500

    All rest changes

--- PostmanCloneUi/Program.cs
+++ PostmanCloneUi/Program.cs
@@ -11,7 +11,7 @@ namespace PostmanCloneUi
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Dashbouard());
+            Application.Run(new Dashboard());
         }
     }
 }
